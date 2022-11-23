2a15abef9693b8cc83218daf2e562c787fa780a6
Matilde Marazzini <marazzinimatilde@gmail.com>
2022-11-23 11:46:20 +0100

    homework

--- myfirstcommit.R
+++ myfirstcommit.R
@@ -14,3 +14,31 @@ library(tidyverse)
 #Or you can do this in RStudio by clicking on the icon git (left, above) and click push
 
 #Pull -----
+#you can download the changes made by someone on github
+#click the command in the git icon
+
+#Git table left above ----
+#yellow symbol = untracked, changes won't be in the next commit
+#green symbol = staged, the script is ready for next commit
+#blue symbol = modified, the change is stored in your computer
+
+#status ----
+#when working in group is good to check if anyone changed something. 
+#You can do it by checking the status in the shell $ git status
+#or you can simply pull and see if there are changes
+
+for (i in 1:5) {
+  print(i)
+}
+
+#HOMEWORK ----
+library(tidyverse)
+view(simpsons)
+str(simpsons)
+class(simpsons)
+#it's a list
+
+
+for (i in simpsons) {
+  print(i$title)
+}
